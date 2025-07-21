034737adbe4993796bf9f03252f44a167aa8af61
Penn <lemonmonlucy@163.com>
2025-07-21 21:17:11 +0800

    fix: fixed views static

--- src/middleware.js
+++ src/middleware.js
@@ -7,7 +7,7 @@ export function middleware(request, event) {
   async function sendAnalytics() {
     const URL =
       process.env.NODE_ENV === 'production'
-        ? 'https://pennlam.com/api/increment-views'
+        ? `${request.nextUrl.origin}/api/increment-views`
         : 'http://localhost:3000/api/increment-views'
 
     try {
